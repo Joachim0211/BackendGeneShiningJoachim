0b428efac4e837e89dd98ba250a245398a446600
Joachim <joachim@joachim-Lenovo-G50-70@gwdg.de>
2021-07-21 11:58:17 +0200

    first steps

--- index.js
+++ index.js
@@ -16,19 +16,20 @@ const readGeneData = (geneDataPath) => {
         // [1] a verbal expression for the function of the gene
         // [2] the length of the gene in base pairs (of DNA)
         // [3] a number that represents the intensity of expression (activity) of the gene under normal (optimal) conditions
-        // [4] a number that indicates the statistical probability that the gene activity is altered in the analysed organism (compared to optimal); 1 = with 100% certainty not different, 0.05 means different with 95% probability.
-        // [5] the extent of the change compared to [3] expressed as base-2 logarithm (convention)
+        // [4] a number that indicates the statistical probability that the gene activity is altered in the analysed organism (compared to optimal); 1 = with 100% certainty not different, 0.05 means different with 95% probability. FDR = False discovery rate
+        // [5] the extent of the change compared to [3] expressed as base-2 logarithm (convention) log2fc
 
-        let [id, definition, length, intensity, probability, change] = parts;
-        change = change.replace("\r\n", "");
+        let [id, biofunct, length, rpkm, fdr, log2fc] = parts;
+        log2fc = log2fc.replace(/[\n\r]+/g, "");
         const lineData = {
           id,
-          definition,
-          length: parseInt(length, 10),
-          intensity: parseInt(intensity, 10),
-          probability: parseInt(probability, 10),
-          change: parseInt(change, 10),
+          biofunct,
+          length, //: parseInt(length, 10),
+          rpkm, //: //parseInt(intensity, 10),
+          fdr,
+          log2fc, //: Number(),
         };
+        
         result.push(lineData);
       });
       s.on("finish", () => {
@@ -42,6 +43,13 @@ const readGeneData = (geneDataPath) => {
   });
 };
 
-readGeneData("./tomatofull.txt")
-  .then((data) => console.log(data))
+readGeneData("txtFiles/fiveDayKdepl.txt")
+  .then(
+    (data) => fs.writeFile("./Mock-data/fiveDayKdepl.json",  JSON.stringify(data), function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data)
+    })
+    ) 
   .catch((e) => console.log(e.message));
---
+++ specialGeneSelector.js
@@ -0,0 +1,25 @@
+
+
+//switch;
+
+const basis = "Kdeficiency.json";
+
+
+//the according file is read into the variable file
+ const fileA = require("./Mock-data/" + basis);
+ const fileB = require("./Mock-data/" + basis);
+ //console.log(file)
+ let numObj = 0;
+
+for(let i = 0; i < fileA.length; i++) {
+    /*for(let i = 0; i < fileB.length; i++){
+
+
+    }*/
+
+    let obj = fileA[i];
+    numObj++;
+    //console.log(obj.biofunct);
+    //let temp = obj.log2fc * obj.
+};
+console.log(numObj);
